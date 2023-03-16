71a77a0ef9a298349baf7ef028a23f55f3382ed7
victor yeswa <victoryeswa01@gmail.com>
2023-03-16 14:44:01 +0300

    Create static_method.py

---
+++ static_method.py
@@ -0,0 +1,14 @@
+class Math:
+
+    @staticmethod
+    def add5(x):
+        return x+5
+    @staticmethod
+    def add10(x):
+        return x +10
+    
+    @staticmethod
+    def pr():
+        print('run')
+
+print(Math.add10(5))
