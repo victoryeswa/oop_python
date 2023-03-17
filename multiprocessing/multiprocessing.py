e90fa7382e8248777b1eea59559a2e1fcd131c32
victor yeswa <victoryeswa01@gmail.com>
2023-03-17 15:56:24 +0300

    Create multiprocessing.py

---
+++ multiprocessing/multiprocessing.py
@@ -0,0 +1,16 @@
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Done sleeping')
+
+
+do_something()
+do_something()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
