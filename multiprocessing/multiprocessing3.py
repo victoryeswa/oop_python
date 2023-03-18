f992b0e5d3df91a92302736737e9a30856973a0e
victor yeswa <victoryeswa01@gmail.com>
2023-03-18 18:16:49 +0300

    Create multiprocessing3.py

---
+++ multiprocessing/multiprocessing3.py
@@ -0,0 +1,26 @@
+#why use multi processing
+#different tasks running in parallel
+
+import multiprocessing
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 second....')
+    time.sleep(1)
+    print('Done sleeping...')
+
+p1 = multiprocessing.Process(target=do_something)
+p2 = multiprocessing.Process(target=do_something)
+
+p1.start()
+p2.start()
+
+p1.join()
+p2.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
+
