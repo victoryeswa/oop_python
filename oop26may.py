bebdca2cdd356d44b66a6dbc825bb9e6850585ef
victor yeswa <victoryeswa01@gmail.com>
2023-05-26 12:22:46 +0300

    Create oop26may.py

---
+++ oop26may.py
@@ -0,0 +1,23 @@
+import turtle
+
+class Polygon:
+    def __init__(self, sides, name):
+        self.sides = sides
+        self.name = name
+        self.interior_angles = (self.sides-2)* 180
+        self.angle = self.interior_angles/self.sides
+
+    def draw(self):
+        for i in range(self, sides):
+            turtle.forward(100)
+            turtle.right(90)
+        turtle.done()
+
+square = Polygon(4, 'Square')
+pentagon = Polygon(5, 'Pentagon')
+
+print(square.sides)
+print(square.name)
+
+print(pentagon.sides)
+
