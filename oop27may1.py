33f9f75baa5aa013d21eba98b96a28d8f24e9e87
victor yeswa <victoryeswa01@gmail.com>
2023-05-27 08:36:14 +0300

    Create oop27may1.py

---
+++ oop27may1.py
@@ -0,0 +1,48 @@
+import csv
+
+class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
+    def __init__(self, name: str, price: float, quantity=0):
+        #Runs Validations to the received arguments
+        assert price >= 0, f'{price} is not greater than or equal to zero!'
+        assert quantity >= 0, f'Quantity {quantity} is not greater than or equal to zero!'
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate 
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name = item.get('name'), 
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity')), 
+
+            )
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
+Item.instantiate_from_csv()
+
+print(Item.all)
+
+
